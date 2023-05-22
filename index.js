61a7f0d29eb6c722277f6f2bda69412740166dff
isyel <isykaal4saviour@gmail.com>
2023-05-22 14:04:52 +0100

    restructure server codes

--- index.js
+++ index.js
@@ -4,120 +4,22 @@
  */
 
 // Dependencies
+const server = require("./lib/server");
+const workers = require("./lib/workers");
 
-const http = require("http");
-const https = require("https");
-const url = require("url");
-const StringDecoder = require("string_decoder").StringDecoder;
-var config = require("./lib/config");
-var fs = require("fs");
-// var _data = require("./lib/data");
+// Declare the app
+const app = {};
 
-// // TESTING
-// // @TODO delete this
-// _data.delete("test", "newFile", (err) => {
-//   console.log("this was the error", err);
-// });
-var handlers = require("./lib/handlers");
-const helpers = require("./lib/helpers");
-
-// @TODO get rid of this
-helpers.sendTwilioSms("8141841767", "Hello!", (err) => {
-  console.log("this was the error", err);
-});
-
-// instantiate the http server
-const httpServer = http.createServer((req, res) => {
-  unifiedServer(req, res);
-});
-
-// Start the server
-httpServer.listen(config.httpPort, () => {
-  console.log(`The server is listening on port ${config.httpPort}`);
-});
-
-// Instantiate the https server
-const httpsServerOptions = {
-  key: fs.readFileSync("./https/key.pem"),
-  cert: fs.readFileSync("./https/cert.pem"),
-};
-const httpsServer = https.createServer(httpsServerOptions, (req, res) => {
-  unifiedServer(req, res);
-});
-
-// Start the HTTPS server
-httpsServer.listen(config.httpsPort, () => {
-  console.log(`The server is listening on port ${config.httpsPort}`);
-});
-
-// All the server logic for both the http and https server
-const unifiedServer = (req, res) => {
-  // get the url and parse it
-  const parsedUrl = url.parse(req.url, true);
-  // get the path
-  const path = parsedUrl.pathname;
-  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
-
-  // Get the query string as an object
-  const queryStringObject = parsedUrl.query;
-
-  // Get the HTTP method
-  const method = req.method.toLowerCase();
-
-  // Get the headers as an object
-  const headers = req.headers;
-
-  // Get the payload, if any
-  const decoder = new StringDecoder("utf-8");
-  let buffer = "";
-  req.on("data", (data) => {
-    buffer += decoder.write(data);
-  });
-  req.on("end", () => {
-    buffer += decoder.end();
-
-    // Choose the handler this request should go to. If one is not found, use the notFound handler
-    const chosenHandler =
-      typeof router[trimmedPath] !== "undefined"
-        ? router[trimmedPath]
-        : handlers.notFound;
-
-    // Construct the data object to send to the handler
-    const data = {
-      trimmedPath,
-      queryStringObject,
-      method,
-      headers,
-      payload: helpers.parseJsonToObject(buffer),
-    };
-
-    // Route the request to the handler specified in the router
-    chosenHandler(data, (statusCode, payload) => {
-      // Use the status code called back by the handler, or default to 200
-      statusCode = typeof statusCode == "number" ? statusCode : 200;
-
-      // Use the payload called back by the handler, or default to an empty object
-      payload = typeof payload == "object" ? payload : {};
-
-      // Convert the payload to a string
-      const payloadString = JSON.stringify(payload);
-
-      // Return the response
-      // res.setHeader("Content-Type", "application/json");
-      res.writeHead(statusCode);
-      // send the response
-      res.end(payloadString);
-
-      // Log the request path
-      console.log("Returning this response: ", statusCode, payloadString);
-    });
-  });
+// Init function
+app.init = () => {
+  // Start the server
+  server.init();
+  // Start the workers
+  workers.init();
 };
 
-// Define a request router
-const router = {
-  ping: handlers.ping,
-  users: handlers.users,
-  tokens: handlers.tokens,
-  checks: handlers.checks,
-};
+// Execute
+app.init();
+
+// Export the app
+module.exports = app;
