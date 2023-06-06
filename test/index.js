dfc004cb6747636e8c631e9c0a88bf312922b6a6
isyel <isykaal4saviour@gmail.com>
2023-06-06 21:59:01 +0100

    correctly manage tests run

--- test/index.js
+++ test/index.js
@@ -11,7 +11,7 @@ _app = {};
 
 // Container for the tests
 _app.tests = {
-  unit: require("./unit"),
+  unit: {},
 };
 
 // Assert that the getANumber function is returning a number
@@ -33,6 +33,22 @@ _app.tests.unit["helpers.getANumber should return 2"] = (done) => {
   const val = helpers.getANumber();
   assert.equal(val, 2);
   done();
+};
+
+// Count all the tests
+_app.countTests = () => {
+  let counter = 0;
+  for (let key in _app.tests) {
+    if (_app.tests.hasOwnProperty(key)) {
+      const subTests = _app.tests[key];
+      for (let testName in subTests) {
+        if (subTests.hasOwnProperty(testName)) {
+          counter++;
+        }
+      }
+    }
+  }
+  return counter;
 };
 
 // Run all the tests, collecting the errors and successes
@@ -77,6 +93,34 @@ _app.runTests = () => {
       }
     }
   }
+};
+
+// Product a test outcome report
+_app.produceTestReport = (limit, successes, errors) => {
+  console.log("");
+  console.log("--------BEGIN TEST REPORT--------");
+  console.log("");
+  console.log("Total Tests: ", limit);
+  console.log("Pass: ", successes);
+  console.log("Fail: ", errors.length);
+  console.log("");
+
+  // If there are errors, print them in detail
+  if (errors.length > 0) {
+    console.log("--------BEGIN ERROR DETAILS--------");
+    console.log("");
+    errors.forEach((testError) => {
+      console.log("\x1b[31m%s\x1b[0m", testError.name);
+      console.log(testError.error);
+      console.log("");
+    });
+    console.log("");
+    console.log("--------END ERROR DETAILS--------");
+  }
+
+  console.log("");
+  console.log("--------END TEST REPORT--------");
+  process.exit(0);
 };
 
 // Run the test
